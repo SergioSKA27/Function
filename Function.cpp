9b7034ecffc8d90d9128f02813b253e558a0e720
Sergio Lopez <sergioska81@hotmail.com>
2022-05-01 18:50:04 -0500

    Primer commit :)

---
+++ Function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <streambuf>
+#include <bit>
+
+class Function
+{
+private:
+    std::string name;                        // name of the function
+    std::vector<std::string> variable_names; // vector for the name of the variables
+    std::string expresion;                   // normal representation of the function
+public:
+    Function(/* args */);
+    ~Function();
+};
+
+Function::Function(/* args */)
+{
+}
+
+Function::~Function()
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
